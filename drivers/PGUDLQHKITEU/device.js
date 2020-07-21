8b3d3077df5fdb0df45aebe389a912f810b3f9ea
Petter <petterruud@gmail.com>
2020-07-21 20:11:49 +0200

    added pergo quickheat

---
+++ drivers/PGUDLQHKITEU/device.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ScinanDevice = require('../../lib/ScinanDevice.js');
+
+class PGUDLQHKITEUDevice extends ScinanDevice {}
+
+module.exports = PGUDLQHKITEUDevice;
---
+++ drivers/PGUDLQHKITEU/driver.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ScinanDriver = require('../../lib/ScinanDriver.js');
+
+class PGUDLQHKITEUDriver extends ScinanDriver {}
+
+module.exports = PGUDLQHKITEUDriver;
